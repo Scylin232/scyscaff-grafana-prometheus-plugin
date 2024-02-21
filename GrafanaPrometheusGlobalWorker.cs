dd696a0bf3353c0df5cd7178816ab8b67e1da7df
Scylin232 <timyr3@ukr.net>
2024-02-21 02:28:25 +0100

    Initial Commit;

---
+++ GrafanaPrometheusGlobalWorker.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using ScyScaff.Core.Models.Plugins;
+using ScyScaff.Docker.Enums;
+using ScyScaff.Docker.Models.Builder;
+using ScyScaff.Docker.Models.Plugins;
+
+namespace ScyScaffPlugin.GrafanaPrometheusGlobalWorker;
+
+public class GrafanaPrometheusGlobalWorker : IGlobalWorkerPlugin, IDockerCompatible
+{
+    public string GlobalWorkerName { get; } = "grafana-prometheus";
+
+    public string GetTemplateTreePath() => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
+    
+    public IEnumerable<DockerComposeService> GetComposeServices(string projectName, string serviceName, int serviceIndex)
+    {
+        List<DockerComposeService> dockerComposeServices = new();
+
+        DockerComposeService prometheusService = new DockerComposeService
+        {
+            Type = DockerComposeServiceType.GlobalWorker,
+            Image = "prom/prometheus",
+            ContainerName = "prometheus",
+            Ports = new Dictionary<int, int?>
+            {
+                { 9090, 9090 }
+            },
+            Volumes = new Dictionary<string, string>
+            {
+                { "./prometheus", "/etc/prometheus" },
+                { "prometheus-data", "/prometheus" }
+            }
+        };
+
+        DockerComposeService grafanaService = new DockerComposeService
+        {
+            Type = DockerComposeServiceType.GlobalWorker,
+            Image = "grafana/grafana",
+            ContainerName = "grafana",
+            Ports = new Dictionary<int, int?>
+            {
+                { 3000, 3000 }
+            }
+        };
+        
+        dockerComposeServices.Add(prometheusService);
+        dockerComposeServices.Add(grafanaService);
+        
+        return dockerComposeServices;
+    }
+}
